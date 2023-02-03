5f6166a5031140bb9e0d7e402250163812b7bdfc
Your Name <parker.josie.eden@gmail.com>
2023-02-03 08:55:25 -0600

    finished with game, everything is working

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 let score = 0;
+
+let displayScore = document.getElementById('score');
+
+let holes = document.getElementsByClassName('hole');
+
+//put the mole on a random hole:
+setInterval(function (){
+    let randomHoleIdx = Math.floor(Math.random() * holes.length);
+    holes[randomHoleIdx].classList.toggle('mole');
+}, 300);
+
+//event listener:
+const gameArea = document.getElementById('whack-a-mole');
+gameArea.addEventListener('click', function(clickEvent){
+    if (clickEvent.target.matches('.mole')){
+        clickEvent.target.classList.remove('mole');
+        score++;
+        displayScore.innerText = score;
+    }
+})
